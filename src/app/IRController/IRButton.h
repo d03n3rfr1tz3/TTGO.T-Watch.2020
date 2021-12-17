883625182e546511db89f55ea92039166160f751
Dirk Sarodnick <github@dirk-sarodnick.de>
2021-12-17 04:25:04 +0100

    added multi-command support to IR; also tried to refactor, that IR buttons get allocated dynamically, but broke something in the last few minutes and it is too late to figure out, what went wrong

--- src/app/IRController/IRButton.h
+++ src/app/IRController/IRButton.h
@@ -11,67 +11,110 @@
         #include <IRremoteESP8266.h>
         #define RAW_CODE_BUFFER_SIZE sizeof(uint16_t)*120
 
+        struct InfraCommand
+        {
+            ~InfraCommand() {
+                if (raw != nullptr) free(raw);
+                raw = nullptr;
+            }
+
+            decode_type_t mode;
+            uint32_t code = 0;
+            int bits = 0;
+            uint16_t *raw = nullptr;
+            int rawLength = 0;
+
+            void resize(int size)
+            {
+                if (raw == nullptr) 
+                raw = (uint16_t*)MALLOC(RAW_CODE_BUFFER_SIZE);
+                rawLength = size;
+                memset(raw, 0, sizeof(uint16_t)*rawLength);
+            }
+
+            void loadFrom(JsonObject& source)
+            {
+                mode = (decode_type_t)(int)source["m"];
+                
+                if (source.containsKey("hex")) {
+                    const char* hex = source["hex"];
+                    if (hex != nullptr)
+                        code = strtoul(hex, NULL, 16);
+                }
+
+                if (source.containsKey("bits")) {
+                    bits = source["bits"];
+                } else if (mode == decode_type_t::SONY && source.containsKey("hex")) {
+                    const char* hex = source["hex"];
+                    size_t hexLength = strlen(hex);
+                    if (hexLength == 3) {
+                        bits = 12;
+                    } else if (hexLength == 4) {
+                        bits = 15;
+                    } // otherwise sony default length of 20 will be used.
+                }
+
+                if (mode == decode_type_t::RAW && source.containsKey("raw")) {
+                    JsonArrayConst arr = source["raw"].as<JsonArray>();
+                    if (!arr.isNull()) {
+                        log_d("RAW size: %d", arr.size());
+                        resize(arr.size());
+                        for (int i=0; i < arr.size(); i++)
+                        raw[i] = arr[i].as<uint16_t>();
+                    }
+                }
+            }
+        };
+
         struct InfraButton
         {
-        ~InfraButton() {
-            free(raw);
-            raw = nullptr;
-        }
-        size_t page;
-        String name;
-        decode_type_t mode;
-        uint32_t code = 0;
-        int bits = 0;
-        uint16_t *raw = nullptr;
-        int rawLength = 0;
-        Button uiButton;
-
-        void resize(int size)
-        {
-            if (raw == nullptr)
-            raw = (uint16_t*)MALLOC(RAW_CODE_BUFFER_SIZE);
-            rawLength = size;
-            memset(raw, 0, sizeof(uint16_t)*rawLength);
-        }
-        void loadFrom(JsonObject& source)
-        {
-            mode = (decode_type_t)(int)source["m"];
-            
-            if (source.containsKey("rn")) {
-            const char* renamed = source["rn"];
-            if (name != renamed && uiButton.isCreated())
-                uiButton.text(renamed);
-            name = renamed;
+            ~InfraButton() {
+                if (commands != nullptr) free(commands);
+                commands = nullptr;
             }
 
-            if (source.containsKey("hex")) {
-            const char* hex = source["hex"];
-            if (hex != nullptr)
-                code = strtoul(hex, NULL, 16);
+            size_t page;
+            char name[16];
+            Button uiButton;
+            InfraCommand* commands = nullptr;
+            size_t commandCount = 0;
+
+            void loadFrom(JsonObject& source) {
+                commandCount = 1;
+                commands = (InfraCommand*)MALLOC(sizeof( InfraCommand ) * commandCount);
+
+                if (commands) {
+                    commands[0].loadFrom(source);
+                }
+                else {
+                    log_e("alloc infrared commands failed");
+                    while( true );
+                }
             }
 
-            if (source.containsKey("bits")) {
-            bits = source["bits"];
-            } else if (mode == decode_type_t::SONY && source.containsKey("hex")) {
-            const char* hex = source["hex"];
-            size_t hexLength = strlen(hex);
-            if (hexLength == 3) {
-                bits = 12;
-            } else if (hexLength == 4) {
-                bits = 15;
-            } // otherwise sony default length of 20 will be used.
-            }
+            void loadFrom(JsonArray& source) {
+                commandCount = source.size();
+                commands = (InfraCommand*)MALLOC(sizeof( InfraCommand ) * commandCount);
 
-            if (mode == decode_type_t::RAW && source.containsKey("raw")) {
-            JsonArrayConst arr = source["raw"].as<JsonArray>();
-            if (!arr.isNull()) {
-                log_d("RAW size: %d", arr.size());
-                resize(arr.size());
-                for (int i=0; i < arr.size(); i++)
-                raw[i] = arr[i].as<uint16_t>();
+                if (commands) {
+                    for (size_t i = 0; i < commandCount; i++) {
+                        JsonObject sourceObject = source[i].as<JsonObject>();
+                        commands[i].loadFrom(sourceObject);
+                        
+                        // Process renaming property for compatibility reasons
+                        if (sourceObject.containsKey("rn")) {
+                            const char* renamed = sourceObject["rn"];
+                            if (name != renamed && uiButton.isCreated())
+                                uiButton.text(renamed);
+                            strncpy( name, renamed, sizeof( name ) );
+                        }
+                    }
+                }
+                else {
+                    log_e("alloc infrared commands failed");
+                    while( true );
+                }
             }
-            }
-        }
         };
     #else // NEW_HARDWARE_TAG
     #endif
